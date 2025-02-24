f6635a09c304864ad915fbde7fb2ea45b3614b52
TARUC <TARUC@B93>
2025-02-24 09:49:37 +0800

    complete

--- src/main/java/com/mycompany/agilew1/AgileW1.java
+++ src/main/java/com/mycompany/agilew1/AgileW1.java
@@ -13,5 +13,6 @@ public class AgileW1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Thank You!");
+        System.out.println("Birthday!");
     }
 }
